d312678d841310fb7d04747d2c506dc34fdfe878
Jens Krüger <jens.krueger@uni-due.de>
2020-06-04 02:30:07 +0200

    add consts for each color to make the color array more readable and give access to each color by name

--- cpp/OpenGL/10/TetrisConst.h
+++ cpp/OpenGL/10/TetrisConst.h
@@ -5,10 +5,7 @@
 #include <Vec3.h>
 #include <Vec2.h>
 
-
-std::array<Vec2,4> test{Vec2{-1,0},Vec2{0,0},Vec2{1,0},Vec2{2,0}};
-
-std::vector<std::vector<std::vector<Vec2>>> tetrominos{	
+const std::vector<std::vector<std::array<Vec2,4>>> tetrominos{	
 		{{Vec2{-1,0},Vec2{0,0},Vec2{1,0},Vec2{2,0}},
 		 {Vec2{1,-1},Vec2{1,0},Vec2{1,1},Vec2{1,2}},
 		 {Vec2{-1,1},Vec2{0,1},Vec2{1,1},Vec2{2,1}},
@@ -36,13 +33,22 @@ std::vector<std::vector<std::vector<Vec2>>> tetrominos{
 		 {Vec2{1,-1},Vec2{1,0},Vec2{0,0},Vec2{0,1}}}  		 // Z, 4 orientations
 };
 
-std::vector<Vec3> colors{
-		Vec3{0,1,1},		// cyan
-		Vec3{1,1,0},		// yellow
-		Vec3{1,0,1},		// magenta
-		Vec3{0,0,1},		// blue
-		Vec3{0.6,0.15,0},	// brown
-		Vec3{0,1,0},		// green
-		Vec3{1,0,0},  	   	// red
-		Vec3{0,0,0}			// black
+const Vec3 cyan{0,1,1};
+const Vec3 yellow{1,1,0};
+const Vec3 magenta{1,0,1};
+const Vec3 blue{0,0,1};
+const Vec3 brown{0.6,0.15,0};
+const Vec3 green{0,1,1};
+const Vec3 red{1,0,0};
+const Vec3 black{0,0,0};
+
+const std::vector<Vec3> colors{
+	cyan,
+	yellow,
+	magenta,
+	blue,
+	brown,
+	green,
+	red,
+	black
 };
