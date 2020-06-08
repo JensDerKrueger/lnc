13a9f65273145681700897c4b1d29a2176769360
Jens Krüger <jens.krueger@uni-due.de>
2020-06-08 22:46:33 +0200

    add time as parameter

--- cpp/OpenGL/10/Grid.h
+++ cpp/OpenGL/10/Grid.h
@@ -15,7 +15,7 @@ public:
 	Grid(std::shared_ptr<Renderer> renderer);
 	
 	void render(const std::array<Vec2i,4>& tetrominoPos, const Vec3& currentColor, const std::array<Vec2i,4>& nextTetrominoPos, const Vec3& nextColor,
-				const std::array<Vec2i,4>& targerTetrominoPos) const;
+				const std::array<Vec2i,4>& targerTetrominoPos, float time) const;
 	void clear();
 	void setPixel(uint32_t x, uint32_t y, int8_t val);
 	int8_t getPixel(uint32_t x, uint32_t y) const;
@@ -25,6 +25,7 @@ public:
 	uint32_t getWidth() const {return width;}
 	uint32_t getHeight() const {return height;}
 	
+	std::shared_ptr<Renderer> getRenderer() const {return renderer;}
 private:
 	uint32_t width;
 	uint32_t height;
