a10301d4d4f873408af9ba7e1e27bdd592010fb1
Jens Krüger <jens.krueger@uni-due.de>
2020-07-11 00:24:32 +0200

    remove unused var

--- cpp/OpenGL/terrain/terrain-vs.glsl
+++ cpp/OpenGL/terrain/terrain-vs.glsl
@@ -4,7 +4,6 @@ uniform mat4 MV;
 uniform mat4 itMV;
 in vec3 vPos;
 in vec3 vNormal;
-in vec4 vColor;
 out vec4 color;
 out vec3 normal;
 out vec3 pos;
@@ -15,5 +14,4 @@ void main() {
     normal = (itMV * vec4(vNormal, 0.0)).xyz;
     height = vPos.y;
     pos = (MV * vec4(vPos, 1.0)).xyz;
-    color = vColor;
 }
