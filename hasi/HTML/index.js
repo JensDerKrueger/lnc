0d89963496d5b5468b6fa12de0d9561bcb16e076
Jens Krüger <jens.krueger@uni-due.de>
2020-08-09 14:58:24 +0200

    remove test code

--- hasi/HTML/index.js
+++ hasi/HTML/index.js
@@ -1,21 +1,5 @@
 function honk(melody) {
   setValue("CH.1",melody)
-
-  /*
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-          x(xmlHttp.responseText);
-  }
-  xmlHttp.open("GET", "http://134.91.11.186:11111/set?variable=CH.1&value="+melody, true);
-  xmlHttp.send(null);
-
-
-  function honkCallback(info) {
-    // nothing yet
-  }
-
-  */
 }
 
 function updatePage() {
