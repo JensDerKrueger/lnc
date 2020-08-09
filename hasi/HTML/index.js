903259f22d8467afc5ed617afc817255794bd56a
Jens Krüger <jens.krueger@uni-due.de>
2020-08-09 15:46:36 +0200

    add graphs

--- hasi/HTML/index.js
+++ hasi/HTML/index.js
@@ -1,3 +1,13 @@
+function pageLoad() {
+  refreshHASValues();
+  window.addEventListener('resize', recomputeGraphs);
+  recomputeGraphs();
+}
+
+function recomputeGraphs(){
+  createGraph("tempGraph", tempData, "LNC Temperaturhistorie", unescape("%B0C"), 1, 50);
+}
+
 function honk(melody) {
   setValue("CH.1",melody)
 }
