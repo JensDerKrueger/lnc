18c20a6900342a2d54c6224efb0bb4b521ee8c39
Jens Krüger <jens.krueger@uni-due.de>
2020-08-11 18:39:01 +0200

    move data into createTable function

--- hasi/HTML/detail.js
+++ hasi/HTML/detail.js
@@ -4,6 +4,11 @@ function pageLoad() {
 }
 
 function recomputeGraphs(){
-  createGraph("tempGraph", tempData, "LNC Temperaturhistorie", unescape("%B0C"), 20, 40, 1, 60, 0 );
-  createGraph("humidGraph", humidData, "LNC Feuchtehistorie", "%", 30, 70, 1, 60, 0 );
+  var tempData = [ /*append_tempData_here*/ ];
+  var humidData = [ /*append_humidData_here*/ ];
+  var queueData = [ /*append_queueData_here*/ ];
+
+  createGraph("tempGraph", tempData, "Temperaturhistorie", unescape("%B0C"), 20, 40, 1, 60, 0 );
+  createGraph("humidGraph", humidData, "Feuchtehistorie", "%", 30, 70, 1, 60, 0 );
+  createGraph("queueGraph", queueData, "Script Queue Size", "", 0, 10, 1, 60 );
 }
