ea2ecfaf56f80ba9178bb1341b807fff586d9358
Jens Krüger <jens.krueger@uni-due.de>
2020-08-18 00:43:18 +0200

    GPU version of game of life

--- cpp/OpenGL/GameOfLife/evolveFS.glsl
+++ cpp/OpenGL/GameOfLife/evolveFS.glsl
@@ -1,25 +1,41 @@
 #version 410
 
 uniform sampler2D gridSampler;
+uniform vec2 paintPos;
 
 in vec2 tc;
 out vec4 FragColor;
 
 void main() {
-  float gridValue = texture(gridSampler, tc).r;
-  
-  float left = texture(gridSampler, tc + vec2(-1,0)).r;
-  float right = texture(gridSampler, tc + vec2(1,0)).r;
-  float top = texture(gridSampler, tc + vec2(0,1)).r;
-  float bottom = texture(gridSampler, tc + vec2(0,-1)).r;
-  
-  float n = left+right .....
-  
-  float result;
-  if (gridValue == 1)
-    result =  round(n) == 2.0 || n == 3.0 ? 1.0 : 0.0;
-  else
-    result = n == 3.0 ? 1.0 : 0.0;
-  
-  FragColor = vec4(result, result, result, 1.0);
+  vec2 texSize = vec2(textureSize(gridSampler,0));
+  if (paintPos.x < 0) {
+    
+    vec2 pixOffset[8] = vec2[]( vec2(-1,-1), vec2(0,-1), vec2(1,-1),
+                                vec2(-1, 0),             vec2(1, 0),
+                                vec2(-1, 1), vec2(0, 1), vec2(1, 1));
+    
+    float n = 0;
+    for (int i = 0;i<8;i++) {
+      n += texture(gridSampler, tc + pixOffset[i] / texSize).r;
+    }
+
+    float gridValue = texture(gridSampler, tc).r;
+
+    float result;
+    if (gridValue == 1)
+      result = n == 2.0 || n == 3.0 ? 1.0 : 0.0;
+    else
+      result = n == 3.0 ? 1.0 : 0.0;
+    
+    FragColor = vec4(result, result, result, 1.0);
+  } else {
+    float gridValue = texture(gridSampler, tc).r;
+    
+    if (int(tc.x*texSize.x) == int(paintPos.x*texSize.y) &&
+        int(tc.y*texSize.x) == int((1-paintPos.y)*texSize.y)) {
+      gridValue = 1.0;
+    }
+    
+    FragColor = vec4(gridValue, gridValue, gridValue, 1.0);
+  }
 }
--- cpp/OpenGL/GameOfLife/main.cpp
+++ cpp/OpenGL/GameOfLife/main.cpp
@@ -9,72 +9,22 @@
 #include <GLBuffer.h>
 #include <GLArray.h>
 #include <GLProgram.h>
+#include <GLFramebuffer.h>
 
 #include <Tesselation.h>
 
 GLEnv gl{1024,1024,4,"OpenGL Game of Life", true, false, 4, 1, true};
-GLTexture2D gridTexture{GL_NEAREST, GL_NEAREST};
+std::vector<GLTexture2D> gridTextures{GLTexture2D{GL_NEAREST, GL_NEAREST},
+                                      GLTexture2D{GL_NEAREST, GL_NEAREST}};
 GLBuffer vbFullScreenQuad{GL_ARRAY_BUFFER};
 GLBuffer ibFullScreenQuad{GL_ELEMENT_ARRAY_BUFFER};
 GLArray fullScreenQuadArray;
 GLProgram progFullscreenQuad{GLProgram::createFromFile("fullScreenQuadVS.glsl", "fullScreenQuadFS.glsl")};
-//GLProgram progevolve{GLProgram::createFromFile("fullScreenQuadVS.glsl", "evolveFS.glsl")};
 
-class Grid2D {
-public:
-  Grid2D(size_t width, size_t height) :
-    width(width),
-    height(height),
-    data(this->width*this->height) {}
-  
-  void setData(size_t x, size_t y, bool value) {
-    data[index(x,y)] = value;
-  }
-  
-  bool getData(size_t x, size_t y) const {
-    return data[index(x,y)];
-  }
-  
-  size_t getHeight() const {return height;}
-  size_t getWidth() const {return width;}
-  
-  size_t countNeighbours(size_t x, size_t y) const {
-    size_t count = 0;
-    for (int64_t dy = -1;dy<=1;++dy) {
-      for (int64_t dx = -1;dx<=1;++dx) {
-        if (dx==0 && dy == 0) continue;
-        count += getDataCyclic(int64_t(x)+dx, int64_t(y)+dy) ? 1 : 0;
-      }
-    }
-    return count;
-  }
-  
-  
-  std::vector<uint8_t> toByteArray() const {
-    std::vector<uint8_t> byteData(data.size());
-    for (size_t i = 0;i<data.size();++i) {
-      byteData[i] = data[i] ? 255 : 0;
-    }
-    return byteData;
-  }
-  
-private:
-  size_t width;
-  size_t height;
-  std::vector<bool> data;
-  
-  size_t index(size_t x, size_t y) const {
-    return y*width+x;
-  }
-  
-  bool getDataCyclic(int64_t x, int64_t y) const {
-    x = (x+width) % width;
-    y = (y+height) % height;
-    return getData(x,y);
-  }
-  
-  
-};
+GLFramebuffer framebuffer;
+GLProgram progEvolve{GLProgram::createFromFile("fullScreenQuadVS.glsl", "evolveFS.glsl")};
+size_t current = 0;
+Vec2 paintPosition{-1,-1};
 
 static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
   if (action == GLFW_PRESS) {
@@ -86,94 +36,83 @@ static void keyCallback(GLFWwindow* window, int key, int scancode, int action, i
   }
 }
 
-void init(Grid2D& g) {
-  g.setData(g.getWidth()/2+10,g.getHeight()/2+10, true);
-  g.setData(g.getWidth()/2+10,g.getHeight()/2+11, true);
-  g.setData(g.getWidth()/2+10,g.getHeight()/2+ 9, true);
-  g.setData(g.getWidth()/2+ 9,g.getHeight()/2+10, true);
-  g.setData(g.getWidth()/2+11,g.getHeight()/2+ 9, true);
+void paint(GLFWwindow* window) {
+  int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
   
-  gridTexture.setData(g.toByteArray(), g.getWidth(), g.getHeight(), 1);
+  if (state == GLFW_PRESS) {
+    double xPosition, yPosition;
+    glfwGetCursorPos(window, &xPosition, &yPosition);
+    
+    int xsize, ysize;
+    glfwGetWindowSize(window, &xsize, &ysize);
+    paintPosition = Vec2{float(xPosition/xsize),float(yPosition/ysize)};
+  } else if (state == GLFW_RELEASE) {
+    paintPosition = Vec2{-1,-1};
+  }
+  
+}
+
+static void cursorPositionCallback(GLFWwindow* window, double xPosition, double yPosition) {
+  paint(window);
+}
+
+static void mouseButtonCallback(GLFWwindow* window, int button, int state, int mods) {
+  paint(window);
+}
+
+static void scrollCallback(GLFWwindow* window, double x_offset, double y_offset) {
+}
+
+void init() {
+  Vec2 gridSize{512,512};
+
+  gridTextures[0].setEmpty( gridSize.x(), gridSize.y(), 1);
+  gridTextures[1].setEmpty( gridSize.x(), gridSize.y(), 1);
   Tesselation fullScreenQuad{Tesselation::genRectangle({0,0,0},2,2)};
   vbFullScreenQuad.setData(fullScreenQuad.getVertices(),3);
   ibFullScreenQuad.setData(fullScreenQuad.getIndices());
   fullScreenQuadArray.bind();
   fullScreenQuadArray.connectVertexAttrib(vbFullScreenQuad,progFullscreenQuad,"vPos",3);
   fullScreenQuadArray.connectIndexBuffer(ibFullScreenQuad);
-  
-  GLint gridTexLocation{progFullscreenQuad.getUniformLocation("gridSampler")};
-  progFullscreenQuad.enable();
-  progFullscreenQuad.setTexture(gridTexLocation,gridTexture,0);
 }
 
-void setColor(float r, float g, float b) {
-  const uint32_t index = 16 + uint32_t(r*5) +
-                          6 * uint32_t(g*5) +
-                         36 * uint32_t(b*5);
-  std::cout << "\033[48;5;" << index << "m";
-}
-
-void clear() {
+void render() {
   Dimensions dim{gl.getFramebufferSize()};
   GL(glViewport(0, 0, dim.width, dim.height));
-  GL(glClearColor(0.0,1.0,0.0,0.0));
-  GL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
-}
-
-void render(const Grid2D& g) {
-  clear();
-  gridTexture.setData(g.toByteArray(), g.getWidth(), g.getHeight(), 1);
-
   progFullscreenQuad.enable();
+  progFullscreenQuad.setTexture(progFullscreenQuad.getUniformLocation("gridSampler"),gridTextures[current],0);
   fullScreenQuadArray.bind();
   GL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0));
+  progFullscreenQuad.unsetTexture(0);
 }
 
-void evolve(const Grid2D& currentGrid, Grid2D& nextGrid) {
-  // enable next buffer for writing
-  // enable current buffer (texture) for reading
-  // enable evolveShader
-  // render quad
-  // disable next buffer
-  // disable current buffer
-  // swap next and current
-  
-  
-  for (size_t y = 0;y<currentGrid.getHeight();++y) {
-    for (size_t x = 0;x<currentGrid.getWidth();++x) {
-      const size_t n{currentGrid.countNeighbours(x,y)};
-      const bool currentCell{currentGrid.getData(x,y)};
-      
-      if (currentCell)
-        nextGrid.setData(x,y, n == 2 || n == 3);
-      else
-        nextGrid.setData(x,y, n == 3);
-    }
-  }
+void evolve() {
+  framebuffer.bind( gridTextures[1-current] );
+  GL(glViewport(0, 0, gridTextures[1-current].getWidth(), gridTextures[1-current].getHeight()));
+  progEvolve.enable();
+  progEvolve.setUniform(progEvolve.getUniformLocation("paintPos"), paintPosition);
+  progEvolve.setTexture(progEvolve.getUniformLocation("gridSampler"),gridTextures[current],0);
+  fullScreenQuadArray.bind();
+  GL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0));
+  framebuffer.unbind();
+  progEvolve.unsetTexture(0);
+  current = 1-current;
 }
 
 int main(int argc, char** argv) {
-//  gl.setMouseCallbacks(cursorPositionCallback, mouseButtonCallback, scrollCallback);
+  gl.setMouseCallbacks(cursorPositionCallback, mouseButtonCallback, scrollCallback);
   gl.setKeyCallback(keyCallback);
-  
-  const size_t width{512};
-  const size_t height{512};
-    
-  Grid2D currentGrid{width,height};
-  Grid2D nextGrid{width,height};
-  
-  init(currentGrid);
+
+  init();
     
   GLEnv::checkGLError("BeforeFirstLoop");
   do {
-    render(currentGrid);
-    evolve(currentGrid, nextGrid);
-    std::swap(currentGrid, nextGrid);
-//    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    render();
+    evolve();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
     GLEnv::checkGLError("endOfFrame");
     gl.endOfFrame();
   } while (!gl.shouldClose());
-
-    
+  
   return EXIT_SUCCESS;
 }
