196201d816daea776e9b541ac7c8ffd1ab352e57
Jens Krüger <jens.krueger@uni-due.de>
2020-08-18 00:48:03 +0200

    move glviewport into bind!

--- cpp/OpenGL/Utils/GLFramebuffer.cpp
+++ cpp/OpenGL/Utils/GLFramebuffer.cpp
@@ -12,93 +12,109 @@ GLFramebuffer::~GLFramebuffer() {
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t, const GLDepthBuffer& d) {
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
-    GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
 
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t.getId(), 0));
 
-    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-    GL(glDrawBuffers(1, DrawBuffers));
+  GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+  GL(glDrawBuffers(1, DrawBuffers));
+
+  GL(glViewport(0, 0, t.getWidth(), t.getHeight()));
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t0, const GLTexture2D& t1, const GLDepthBuffer& d) {
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
-    GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
 
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
 
-    GLenum DrawBuffers[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
-    GL(glDrawBuffers(2, DrawBuffers));
+  GLenum DrawBuffers[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
+  GL(glDrawBuffers(2, DrawBuffers));
+
+  GL(glViewport(0, 0, t0.getWidth(), t0.getHeight()));
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t0, const GLTexture2D& t1, const GLTexture2D& t2, const GLDepthBuffer& d) {
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
-    GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
 
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
 
-    GLenum DrawBuffers[3] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2};
-    GL(glDrawBuffers(3, DrawBuffers));
+  GLenum DrawBuffers[3] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2};
+  GL(glDrawBuffers(3, DrawBuffers));
+  
+  GL(glViewport(0, 0, t0.getWidth(), t0.getHeight()));
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t0, const GLTexture2D& t1, const GLTexture2D& t2, const GLTexture2D& t3, const GLDepthBuffer& d) {
 
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
-    GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, d.getId()));
 
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT3, t3.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT3, t3.getId(), 0));
 
-    GLenum DrawBuffers[4] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3};
-    GL(glDrawBuffers(4, DrawBuffers));
+  GLenum DrawBuffers[4] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3};
+  GL(glDrawBuffers(4, DrawBuffers));
+  
+  GL(glViewport(0, 0, t0.getWidth(), t0.getHeight()));
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t) {
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
 
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t.getId(), 0));
 
-    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-    GL(glDrawBuffers(1, DrawBuffers));
+  GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+  GL(glDrawBuffers(1, DrawBuffers));
+  
+  GL(glViewport(0, 0, t.getWidth(), t.getHeight()));
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t0, const GLTexture2D& t1) {
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
 
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
 
-    GLenum DrawBuffers[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
-    GL(glDrawBuffers(2, DrawBuffers));
+  GLenum DrawBuffers[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
+  GL(glDrawBuffers(2, DrawBuffers));
+  
+  GL(glViewport(0, 0, t0.getWidth(), t0.getHeight()));
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t0, const GLTexture2D& t1, const GLTexture2D& t2) {
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
 
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
 
-    GLenum DrawBuffers[3] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2};
-    GL(glDrawBuffers(3, DrawBuffers));
+  GLenum DrawBuffers[3] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2};
+  GL(glDrawBuffers(3, DrawBuffers));
+  
+  GL(glViewport(0, 0, t0.getWidth(), t0.getHeight()));
 }
 
 void GLFramebuffer::bind(const GLTexture2D& t0, const GLTexture2D& t1, const GLTexture2D& t2, const GLTexture2D& t3) {
-    GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
+  GL(glBindFramebuffer(GL_FRAMEBUFFER, id));
 
-    GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
-    GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT3, t3.getId(), 0));
+  GL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, t0.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, t1.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, t2.getId(), 0));
+  GL(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT3, t3.getId(), 0));
 
-    GLenum DrawBuffers[4] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3};
-    GL(glDrawBuffers(4, DrawBuffers));
+  GLenum DrawBuffers[4] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3};
+  GL(glDrawBuffers(4, DrawBuffers));
+  
+  GL(glViewport(0, 0, t0.getWidth(), t0.getHeight()));
 }
 
 void GLFramebuffer::unbind() {
