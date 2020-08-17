435bc0e0aeee0e451003052f5098070b6a156399
Jens Krüger <jens.krueger@uni-due.de>
2020-08-18 00:41:48 +0200

    add missing space

--- cpp/OpenGL/Utils/GLDebug.cpp
+++ cpp/OpenGL/Utils/GLDebug.cpp
@@ -4,6 +4,6 @@
 void errorOut(const std::string& statement, const std::string& location,
               uint32_t line, const std::string& file, const GLubyte * errorDesc,
               uint32_t errnum) {
-  std::cerr << "GL error calling" << statement << " " << location <<  " " << line
+  std::cerr << "GL error calling " << statement << " " << location <<  " " << line
             << " (" << file << "):" <<  errorDesc << " (" << errnum << ")" << std::endl;
 }
