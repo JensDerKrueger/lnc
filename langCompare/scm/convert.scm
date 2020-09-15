94c3e2cd80cc03ae2f7619032c2b4b6c1700da94
Jens Krüger <jens.krueger@uni-due.de>
2020-09-15 23:41:24 +0200

    completed scheme implementation

--- langCompare/scm/convert.scm
+++ langCompare/scm/convert.scm
@@ -1,24 +1,36 @@
-(define (reverse-int ls acc) 
+(define digits "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+
+(define (reverse-ls-int ls acc) 
 	(if (null? ls) 
 		acc 
-		(reverse-int (cdr ls) (cons (car ls) acc))))
+		(reverse-ls-int  (cdr ls) (cons (car ls) acc))))
 
-(define find-int
-	(lambda (c l p)
+(define (find-int c l p)
 		(if (char-ci=? c (car l))
 			p
-			(findInt c (cdr l) (+ p 1)))))
-
-
-(define reverse
-	(lambda (s)
-		(list->string (reverse-int (string->list s) '()))))
+			(find-int c (cdr l) (+ p 1))))
+			
+(define (reverse-ls ls)
+		(reverse-ls-int ls '()))
 		
-(define find
-	(lambda (c)
-		(let ((digits (string->list "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"))) (find-int c digits 0))))
+(define (find c)
+		(find-int c (string->list digits) 0))
 
+(define (str-to-int-int ls in-base multi)	
+	(if (null? ls)
+		0
+		(+ (* (find (car ls)) multi) (str-to-int-int (cdr ls) in-base (* multi in-base)))))
 
-; (define convert  
-;	(lambda (inStr inBase outBase) (intToStr (strToInt inStr inBase) outBase) )
-;)
+(define (str-to-int in-str in-base)
+	(str-to-int-int (reverse-ls (string->list in-str)) in-base 1))
+			
+(define (int-to-str-int number out-base)
+	(if (= 0 number)
+		'()		
+		(cons (string-ref digits (remainder number out-base)) (int-to-str-int(quotient number out-base) out-base))))
+		
+(define (int-to-str number out-base)		
+	(list->string (reverse-ls (int-to-str-int number out-base))))
+
+(define (convert in-str in-base out-base)
+	(int-to-str (str-to-int in-str in-base) out-base))
