46c313557bace9a52bce13668273db0c83bd670a
Harasz2020 <67601357+Harasz2020@users.noreply.github.com>
2020-10-08 11:25:55 +0200

    tabs

--- langCompare/pas/convertOperator.pas
+++ langCompare/pas/convertOperator.pas
@@ -6,27 +6,27 @@ const
 
 function find(c : char; str : string) : integer;
 var
-        i : integer;
+	i : integer;
 begin
 	find := 255;
-        for i := 1 to length(str) do if c = str[i] then exit(i-1);
+	for i := 1 to length(str) do if c = str[i] then exit(i-1);
 end;
 
 function reverse(inStr : string) : string;
 var
-        c   : char;
+	c   : char;
 begin
-        reverse := '';
-        for c in inStr do reverse := c + reverse;
+	reverse := '';
+	for c in inStr do reverse := c + reverse;
 end;
 
 function strToInt(str : string; base : integer) : longint;
 var
-        number : longint = 0;
-        multi  : longint = 1;
+	number : longint = 0;
+	multi  : longint = 1;
 	revStr : string;
 	c      : char;
-        digit  : integer;
+	digit  : integer;
 begin
 	revStr := reverse(str);
 	for c in revStr do
@@ -41,39 +41,39 @@ end;
 
 function intToStr(number : longint; outBase : integer) : string;
 begin
-        intToStr := '';
-        repeat
-            intToStr := digits[number mod outBase + 1] + intToStr;
-            number := number div outBase;
-        until number = 0;
+	intToStr := '';
+	repeat
+		intToStr := digits[number mod outBase + 1] + intToStr;
+		number := number div outBase;
+	until number = 0;
 end;
 
 
 operator in (valstr : string; inbase : integer) outVal : longint;
 begin
-        outVal := strToInt(valstr,inbase);
+	outVal := strToInt(valstr,inbase);
 end;
 
 operator in (number : longint; outBase : integer) outStr : string;
 begin
-        outStr := intToStr(number,outBase);
+	outStr := intToStr(number,outBase);
 end;
 
 
 
 function convert(inStr : string; inBase, outBase : integer) : string;
 begin
-        convert := inStr in inBase in outBase;
+	convert := inStr in inBase in outBase;
 end;
 
 
 begin
-        writeln(convert('1234',10,20));
-        writeln('1234' in 10 in 20);
-        writeln(1234 in 20);
-        writeln('1234' in 20);
-        writeln(3 in 2 in 3 in 2 in 3 in 2 in 3 in 2 in 3);
-        readln;
+	writeln(convert('1234',10,20));
+	writeln('1234' in 10 in 20);
+	writeln(1234 in 20);
+	writeln('1234' in 20);
+	writeln(3 in 2 in 3 in 2 in 3 in 2 in 3 in 2 in 3);
+	readln;
 end.
 
 
