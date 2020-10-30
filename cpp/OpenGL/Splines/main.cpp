091387a562c8d2074c89b069611c9a11a9731b96
Jens Krüger <jens.krueger@uni-due.de>
2020-10-30 01:09:30 +0100

    hermite and bezier curves, remove M_PI define

--- cpp/OpenGL/Splines/main.cpp
+++ cpp/OpenGL/Splines/main.cpp
@@ -5,6 +5,7 @@
 class MyGLApp : public GLApp {
 public:
   
+  const float PI = 3.1415926535f;
   const uint32_t maxA = 120;
   uint32_t a = 0;
   float sa = 0;
@@ -18,11 +19,40 @@ public:
   
   virtual void animate() {
     a = (a + 1) % (maxA+1);
-    sa = sinf(float(a)/maxA*2*M_PI);
-    ca = cosf(float(a)/maxA*2*M_PI);
+    sa = sinf(float(a)/maxA*2*PI);
+    ca = cosf(float(a)/maxA*2*PI);
   }
   
-  void drawSegment(Vec2 p0, Vec2 p1, Vec2 p2, Vec2 p3) {
+  void drawHermiteSegment(Vec2 p0, Vec2 m0, Vec2 m1, Vec2 p1) {
+    const size_t maxVal = 100;
+    std::vector<float> curve((maxVal+1)*7);
+  
+    for (size_t i = 0;i<=maxVal;++i) {
+      float t = float(i)/float(maxVal);
+      
+      float h0 = (1.0f-t)*(1.0f-t)*(1.0f+2.0f*t);
+      float h1 = t*(1.0f-t)*(1.0f-t);
+      float h2 = t*t*(1.0f-t);
+      float h3 = (3.0f-2.0f*t)*t*t;
+      
+      curve[i*7+0] = p0.x()*h0+m0.x()*h1+m1.x()*h2+p1.x()*h3;
+      curve[i*7+1] = p0.y()*h0+m0.y()*h1+m1.y()*h2+p1.y()*h3;
+      curve[i*7+2] = 0.0f;
+      
+      curve[i*7+3] = 0.0;
+      curve[i*7+4] = 0.0;
+      curve[i*7+5] = 0.0;
+      curve[i*7+6] = 1.0f;
+    }
+    drawLines(curve, LineDrawType::STRIP);
+    
+    drawPoints({p0.x(),p0.y(),0,1,0,0,0,
+                p0.x()+m0.x(),p0.y()+m0.y(),0,0,0,1,0,
+                p1.x()+m1.x(),p1.y()+m1.y(),0,0,0,1,0,
+                p1.x(),p1.y(),0,1,0,0,0}, 10);
+  }
+  
+  void drawBezierSegment(Vec2 p0, Vec2 p1, Vec2 p2, Vec2 p3) {
     const size_t maxVal = 100;
     std::vector<float> curve((maxVal+1)*7);
   
@@ -55,12 +85,11 @@ public:
     GL(glClear(GL_COLOR_BUFFER_BIT));
 
     const Vec2 p0{-0.5,0.0f};
-    const Vec2 p1{sa*0.4f-0.5f,ca*0.4f};
+    const Vec2 p1{sa*0.5f-0.5f,ca*0.5f};
     const Vec2 p2{0.5f,0.5f};
     const Vec2 p3{0.5f,0.0f};
 
-    drawSegment(p0,p1,p2,p3);
-    
+    drawBezierSegment(p0,p1,p2,p3);
   }
 
 } myApp;
