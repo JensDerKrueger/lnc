264630e670029985275b500645d123b1af117eb0
Jens Krüger <jens.krueger@uni-due.de>
2020-11-05 13:18:41 +0100

    solution color assgnment

--- cpp/OpenGL/17_Raster/main.cpp
+++ cpp/OpenGL/17_Raster/main.cpp
@@ -1,42 +1,34 @@
 #include <GLApp.h>
+#include <Mat4.h>
 
 class MyGLApp : public GLApp {
 public:
   Image image{640,480};
   
-  uint8_t mandelbrot(size_t x, size_t y) {
-    const float dx = 2.8f / image.width, dy = 2.6f / image.height;
-    const float xs = -2.1f, ys = -1.3f;
-    const float cR = xs + dx*x, cI = ys + dy*y;
-    float dR = 0, dI = 0;
-    uint16_t depth = 0;
-    while (depth < 256 && dR*dR+dI*dI < 4) {
-      float t = dR;
-      dR = dR*dR-dI*dI+cR;
-      dI = 2*t*dI+cI;
-      ++depth;
-    }
-    return uint8_t(depth);
-  }
-  
   virtual void init() {
-    glEnv.setTitle("Raster Demo");
-    GL(glDisable(GL_CULL_FACE));
-    GL(glClearColor(0,0,0,0));
+    glEnv.setTitle("Color Picker");
 
     for (uint32_t y = 0;y<image.height;++y) {
       for (uint32_t x = 0;x<image.width;++x) {
-        uint8_t d = mandelbrot(x,y);
-        image.setValue(x,y,0,uint8_t(d));
-        image.setValue(x,y,1,uint8_t(d*3));
-        image.setValue(x,y,2,uint8_t(d*5));
+        const Vec3 rgb = Vec3::hsvToRgb({360*float(x)/image.width,float(y)/image.height,1.0f});
+        image.setNormalizedValue(x,y,0,rgb.x());
+        image.setNormalizedValue(x,y,1,rgb.y());
+        image.setNormalizedValue(x,y,2,rgb.z());
         image.setValue(x,y,3,255);
       }
     }
   }
+  
+  virtual void mouseMove(double xPosition, double yPosition) {
+    Dimensions s = glEnv.getWindowSize();
+    if (xPosition < 0 || xPosition > s.width || yPosition < 0 || yPosition > s.height) return;
+
+    const Vec3 hsv{float(360*xPosition/s.width),float(1.0-yPosition/s.height),1.0f};
+    const Vec3 rgb = Vec3::hsvToRgb(hsv);
+    std::cout << "HSV: " << hsv << " --> RGB: " << rgb << std::endl;
+  }
     
   virtual void draw() {
-    GL(glClear(GL_COLOR_BUFFER_BIT));
     drawImage(image);
   }
 
@@ -45,4 +37,4 @@ public:
 int main(int argc, char ** argv) {
   myApp.run();
   return EXIT_SUCCESS;
-}  
+}
