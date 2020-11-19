dca859e99a41e14a3d916b9441e3468949388d0f
Jens Krüger <jens.krueger@uni-due.de>
2020-11-19 22:08:14 +0100

    add grid display

--- cpp/OpenGL/20_ShapeDesigner/main.cpp
+++ cpp/OpenGL/20_ShapeDesigner/main.cpp
@@ -4,21 +4,15 @@
 #include <Vec4.h>
 #include <Mat4.h>
 
-std::vector<Vec2> test{Vec2{-0.7, 0.16}, Vec2{-0.26, 0.48}, Vec2{-0.26, 0.48}, Vec2{0.34, 0.66}, Vec2{0.34, 0.66}, Vec2{0.68, 0.28}, Vec2{0.68, 0.28}, Vec2{0.6, -0.4}, Vec2{0.6, -0.4}, Vec2{0.04, -0.66}, Vec2{0.04, -0.66}, Vec2{-0.44, -0.58}, Vec2{-0.44, -0.58}, Vec2{-0.02, 0}};
-
-
 class MyGLApp : public GLApp {
 public:
-  
   MyGLApp() :
   GLApp(800,800,4, "Shape Designer", false, false)
-  {
-  }
+  {}
   
   virtual void init() override {
     glEnv.setCursorMode(CursorMode::HIDDEN);
     GL(glClearColor(0,0,0,0));
-    
   }
   
   virtual void mouseMove(double xPosition, double yPosition) override {
@@ -27,23 +21,19 @@ public:
     mousePos = Vec2{float(xPosition/s.width),float(1.0-yPosition/s.height)};
   }
   
-  
   virtual void mouseButton(int button, int state, int mods, double xPosition, double yPosition) override {
     if (button == GLFW_MOUSE_BUTTON_LEFT && state == GLFW_PRESS) {
-      
       if (restartPrimitive) {
         restartPrimitive = false;
       } else {
         if (shape.size() > 0 && shape.size()%2 == 0) shape.push_back(shape[shape.size()-1]);
       }
-      
       shape.push_back(Vec2{float(int(mousePos.x()*gridCells+0.5f)/gridCells) *2.0f-1.0f,
                            float(int(mousePos.y()*gridCells+0.5f)/gridCells) *2.0f-1.0f});
     }
   }
 
   virtual void keyboard(int key, int scancode, int action, int mods) override {
-
     if (action == GLFW_PRESS) {
       switch (key) {
         case GLFW_KEY_ESCAPE:
@@ -75,7 +65,6 @@ public:
     }
   }
   
-  
   void dumpShape() {
     std::cout << "{";
     for (size_t i = 0;i<shape.size();++i) {
@@ -90,11 +79,25 @@ public:
   
   virtual void draw() override{
     GL(glClear(GL_COLOR_BUFFER_BIT));
-    
     Vec2 gridMousePos{float(int(mousePos.x()*gridCells+0.5f)/gridCells),
-                            float(int(mousePos.y()*gridCells+0.5f)/gridCells)};
-    
+                      float(int(mousePos.y()*gridCells+0.5f)/gridCells)};
+
     std::vector<float> glShape;
+    float y = 0.0f;
+    while (y <= 1.0) {
+      float x = 0.0f;
+      while (x <= 1.0) {
+        
+        glShape.push_back(x*2.0f-1.0f); glShape.push_back(y*2.0f-1.0f); glShape.push_back(0.0f);
+        glShape.push_back(0.2f); glShape.push_back(0.2f); glShape.push_back(0.2f); glShape.push_back(1.0f);
+
+        x += 1.0f/gridCells;
+      }
+      y += 1.0f/gridCells;
+    }
+    drawPoints(glShape,5);
+    glShape.clear();
+    
     glShape.push_back(mousePos.x()*2.0f-1.0f); glShape.push_back(mousePos.y()*2.0f-1.0f); glShape.push_back(0.0f);
     glShape.push_back(0.2f); glShape.push_back(0.2f); glShape.push_back(0.2f); glShape.push_back(1.0f);
     glShape.push_back(gridMousePos.x() *2.0f-1.0f); glShape.push_back(gridMousePos.y() *2.0f-1.0f); glShape.push_back(0.0f);
@@ -117,7 +120,6 @@ public:
       glShape.push_back(gridMousePos.x()*2.0f-1.0f); glShape.push_back(gridMousePos.y()*2.0f-1.0f); glShape.push_back(0.0f);
       glShape.push_back(0.2f); glShape.push_back(0.2f); glShape.push_back(0.2f); glShape.push_back(1.0f);
     }
-
     drawLines(glShape, LineDrawType::LIST);
   }
 
@@ -133,5 +135,4 @@ private:
 int main(int argc, char ** argv) {
   myApp.run();
   return EXIT_SUCCESS;
-}  
-  
+}
