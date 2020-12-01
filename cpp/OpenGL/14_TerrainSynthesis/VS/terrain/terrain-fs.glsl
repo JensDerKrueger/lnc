5439ed216a9fd321a6d58b60240808164582ea89
Harasz2020 <67601357+Harasz2020@users.noreply.github.com>
2020-12-01 01:23:02 +0100

    gameOfLife3D main.cpp fixed; VS files refactored; .gitignore modified
    - GOL3D main.cpp: "GLDataType::HALF" instead of "DT_HALF" in 4 lines
    - GOL, GOL3D and terrain refactored to alow utils to be used as a project  (utils.lib)
    - TODO: terrain - heightfield is invisible

---
+++ cpp/OpenGL/14_TerrainSynthesis/VS/terrain/terrain-fs.glsl
@@ -0,0 +1,24 @@
+#version 410
+uniform vec3 lightPos;
+uniform sampler2D heightSampler;
+in vec3 normal;
+in vec3 pos;
+in float gradient;
+in float height;
+uniform float reduction;
+uniform float alpha;
+out vec4 FragColor;
+
+void main() {
+    vec3 lightDir = normalize(lightPos-pos);
+    float diffuse = dot(normalize(normal), lightDir);
+    vec4 texValue = texture(heightSampler, vec2(gradient,height*reduction));
+    
+    vec3 eyeDir = normalize(-pos);
+    vec3 r = reflect(-lightDir, normal);
+    
+    vec3 specularColor = vec3(1.0,1.0,1.0) * pow(max(0.0,dot(eyeDir,r)),12.0);
+    vec3 diffuseColor = texValue.xyz*diffuse;
+    
+    FragColor = vec4(diffuseColor+specularColor*texValue.a,alpha);
+}
