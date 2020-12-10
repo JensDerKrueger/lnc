55c2209130e96a8da4fc0904c8f5826cedf9e787
Jens Krüger <jens.krueger@uni-due.de>
2020-12-10 23:41:02 +0100

    minor fixes

--- cpp/Tests/02_VectorAddition/main.cpp
+++ cpp/Tests/02_VectorAddition/main.cpp
@@ -5,12 +5,24 @@
 #include <chrono>
 typedef std::chrono::high_resolution_clock Clock;
 
+
+struct TestStruct {
+  TestStruct(const std::vector<uint64_t>& v) :
+    foo(v)
+  {
+  }
+  std::vector<uint64_t> foo;
+};
+
 int main(int argc, char ** argv) {
   const size_t repetitions = 3;
   const size_t testCount = 500;
-  const size_t vecLength = 1000000;
+  const size_t vecLength = 100;
   
-  std::vector<uint64_t> test;
+  std::vector<uint64_t> foo(10000);
+  
+  std::vector<TestStruct> test;
+
 
   auto t1 = Clock::now();
   auto t2 = Clock::now();
@@ -20,7 +32,7 @@ int main(int argc, char ** argv) {
     t1 = Clock::now();
     for (size_t t = 0;t<testCount;++t) {
       for (size_t i = 0; i<vecLength; ++i) {
-        test.push_back(i);
+        test.push_back(TestStruct(foo));
       }
     }
     t2 = Clock::now();
@@ -30,7 +42,7 @@ int main(int argc, char ** argv) {
     t1 = Clock::now();
     for (size_t t = 0;t<testCount;++t) {
       for (size_t i = 0; i<vecLength; ++i) {
-        test.emplace_back(i);
+        test.emplace_back(foo);
       }
     }
     t2 = Clock::now();
