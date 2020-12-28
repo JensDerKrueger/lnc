aaeabfed1d92f4e116269a2b0a3fb63a5c82ee2a
Late Night Coding <jens.krueger@uni-due.de>
2020-12-29 00:53:34 +0100

    remove iostream

--- cpp/OpenGL/Vis_01_MS/MS.cpp
+++ cpp/OpenGL/Vis_01_MS/MS.cpp
@@ -1,7 +1,7 @@
+#include<algorithm>
+
 #include "MS.h"
 #include "MS.inl"
-
-#include <iostream>
 
 Isoline::Isoline(const Image& image, uint8_t isovalue, bool useAsymptoticDecider) {
   // TODO: compute isoline using the ms-algorithm and store the line-list in the vertices vector
