3feffc7a47cefb6c23fcd87d3572b858ee2b3b7f
Jens Krüger <jens.krueger@uni-due.de>
2021-02-11 22:49:37 +0100

    formatting

--- cpp/OpenGL/Utils/FontRenderer.cpp
+++ cpp/OpenGL/Utils/FontRenderer.cpp
@@ -17,41 +17,46 @@ Image FontRenderer::render(uint32_t number,
   return render(std::to_string(number), imageFilename, positionFilename);
 }
 
-
 Image FontRenderer::render(const std::string& text,
                            const std::string& imageFilename,
                            const std::string& positionFilename) {
 
   Image numberSource = BMP::load(imageFilename);
-  
+
   std::vector<CharPosition> positions;
   std::ifstream posfile (positionFilename);
   std::string line;
   if (posfile.is_open()) {
-      while ( getline (posfile,line) ) {
-          std::vector<std::string> vals;
-          std::stringstream tokenizer(line);
-          std::string token;
-          while(getline(tokenizer, token, ' ')) {
-              vals.push_back(token);
-          }
-          if (vals.size() == 5)
-            positions.push_back({vals[0][0],
-                                 {uint32_t(stoi(vals[1])),uint32_t(stoi(vals[2]))},
-                                 {uint32_t(stoi(vals[3])),uint32_t(stoi(vals[4]))}});
+    while (getline(posfile,line) ) {
+      std::vector<std::string> vals;
+      std::stringstream tokenizer(line);
+      std::string token;
+      while(getline(tokenizer, token, ' ')) {
+        vals.push_back(token);
       }
-      posfile.close();
+    
+      if (vals.size() == 5) {
+        positions.push_back({vals[0][0],
+                             {uint32_t(stoi(vals[1])),uint32_t(stoi(vals[2]))},
+                             {uint32_t(stoi(vals[3])),uint32_t(stoi(vals[4]))}});
+      } else if (vals.size() == 6) {
+        positions.push_back({' ',
+                             {uint32_t(stoi(vals[2])),uint32_t(stoi(vals[3]))},
+                             {uint32_t(stoi(vals[4])),uint32_t(stoi(vals[5]))}});
+      }
+    }
+    posfile.close();
   }
   
   Vec2ui dims{0,0};
   for (char element : text) {
-      const auto& pos = findElement(element, positions);
-      Vec2ui size = pos.bottomRight-pos.topLeft;
-      dims = Vec2ui{dims.x()+size.x(), std::max(dims.y(), size.y())};
+    const auto& pos = findElement(element, positions);
+    Vec2ui size = pos.bottomRight-pos.topLeft;
+    dims = Vec2ui{dims.x()+size.x(), std::max(dims.y(), size.y())};
   }
   
   Image result{dims.x(),dims.y(),numberSource.componentCount,
-      std::vector<uint8_t>(dims.x()*dims.y()*numberSource.componentCount)};
+  std::vector<uint8_t>(dims.x()*dims.y()*numberSource.componentCount)};
   
   Vec2ui currentPos{0,0};
   for (char element : text) {
