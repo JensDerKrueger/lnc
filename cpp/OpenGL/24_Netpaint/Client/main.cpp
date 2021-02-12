60621a472c9ab1663d1c9fed71f751aaac75df9a
Jens Krüger <jens.krueger@uni-due.de>
2021-02-12 21:39:01 +0100

    build client as native windows app

--- cpp/OpenGL/24_Netpaint/Client/main.cpp
+++ cpp/OpenGL/24_Netpaint/Client/main.cpp
@@ -530,8 +530,30 @@ private:
   std::string userName{""};
 };
 
+#ifdef _WIN32
+INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+#else
 int main(int argc, char ** argv) {
-  MyGLApp myApp;
-  myApp.run();
+#endif
+  try {
+    MyGLApp myApp;
+    myApp.run();
+  } catch (const GLException& e) {
+
+    std::stringstream ss;
+    ss << "Insufficient OpenGL Support " << e.what();
+
+#ifndef _WIN32
+    std::cerr << ss.str().c_str() << std::endl;
+#else
+    MessageBoxA(
+      NULL,
+      ss.str().c_str(),
+      "OpenGL Error",
+      MB_ICONERROR | MB_OK
+    );
+#endif
+    return EXIT_FAILURE;
+  }
   return EXIT_SUCCESS;
 }
