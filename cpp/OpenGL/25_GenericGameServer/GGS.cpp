48f10c3cfc093cd8068618e1cecbf724e568c5de
Jens Krüger <jens.krueger@uni-due.de>
2021-03-05 16:30:51 +0100

    pulled GGS out of main.cpp and added logging capabilities

---
+++ cpp/OpenGL/25_GenericGameServer/GGS.cpp
@@ -0,0 +1,173 @@
+#include "GGS.h"
+
+#include <sstream>
+#include <fstream>
+
+typedef std::chrono::high_resolution_clock Clock;
+
+
+ClientInfo::ClientInfo() {}
+  
+ClientInfo::ClientInfo(uint32_t id, const std::string& name, GameIDs gameID, uint32_t level) :
+  id{id},
+  name{cleanupName(name)},
+  gameID{gameID},
+  level{level},
+  paired{false},
+  partnerID{0}
+{}
+
+ClientInfo::~ClientInfo() {
+  
+}
+
+std::string ClientInfo::cleanupName(const std::string& name) {
+  std::string cName{name};
+  for (size_t i = 0;i<name.length();++i) {
+    cName[i] = cleanupChar(name[i]);
+  }
+  return cName;
+}
+
+char ClientInfo::cleanupChar(char c) {
+  std::string validChars{"01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ(),._ ;:"};
+  if (validChars.find(c) != std::string::npos) return c; else return '_';
+}
+  
+GGS::GGS(short port) :
+  Server(port)
+{
+}
+
+GGS::~GGS() {
+}
+
+void GGS::writeLog(const std::string& str) const {
+  if (logFile.empty()) return;
+  
+  std::ofstream log(logFile);
+  if (log.is_open()) {
+    log << str;
+    log.close();
+  }
+}
+
+void GGS::handleClientConnection(uint32_t id) {
+  std::stringstream ss;
+  ss << "New client " << id << " connected";
+  writeLog(ss.str());
+}
+
+void GGS::handleClientDisconnection(uint32_t id) {
+  ciMutex.lock();
+
+  std::stringstream ss;
+  ss << "Client " << id << " disconnected";
+  writeLog(ss.str());
+
+  const uint32_t partnerID = clientInfos[id].partnerID;
+  if (partnerID != 0) {
+    LostUserMessage l;
+    l.userID = id;
+    sendMessage(l.toString(), partnerID);
+  }
+  clientInfos.erase(id);
+  ciMutex.unlock();
+}
+
+void GGS::handleClientMessage(uint32_t id, const std::string& message) {
+  ciMutex.lock();
+  try {
+    MessageType pt = identifyString(message);
+    switch (pt) {
+      case MessageType::ConnectMessage : {
+        ConnectMessage r(message);
+        r.userID = id;
+        ClientInfo ci{id, r.name, r.gameID, r.level};
+        clientInfos[id] = ci;
+        searchForMatch(id);
+
+        std::stringstream ss;
+        ss << "Client " << id << " reports, name " << r.name << " and interest in game " << uint32_t(r.gameID) << " at level " << r.level;
+        writeLog(ss.str());
+
+        break;
+      }
+      case MessageType::ReadyForNewMessage : {
+        ReadyForNewMessage r(message);
+        clientInfos[id].partnerID = 0;
+        
+        std::stringstream ss;
+        ss << "Client " << id << " reports, ready for new partner";
+        writeLog(ss.str());
+
+        searchForMatch(id);
+        break;
+      }
+      case MessageType::GameMessage : {
+        GameMessage g(message);
+        g.userID = id;
+        sendMessage(g.toString(), clientInfos[id].partnerID);
+        break;
+      }
+      default:
+        break;
+    };
+  } catch (const MessageException& e) {
+    if (showErrors) {
+      std::cerr << "MessageException: " << e.what() << std::endl;
+    }
+  }
+
+  ciMutex.unlock();
+}
+
+void GGS::pair(uint32_t a, uint32_t b) {
+  clientInfos[a].partnerID = b;
+  clientInfos[b].partnerID = a;
+
+  PairedMessage l;
+  l.userID = a;
+  sendMessage(l.toString(), b);
+
+  l.userID = b;
+  sendMessage(l.toString(), a);
+  
+  std::stringstream ss;
+  ss << "Clients " << a << " and " << b << " are now connected, and playing game " << uint32_t(clientInfos[b].gameID);
+  writeLog(ss.str());
+}
+
+void GGS::searchForMatch(uint32_t id) {
+  if (clientInfos.size() < 2) return;
+  
+  uint32_t match{0};
+  uint32_t minAbs{std::numeric_limits<uint32_t>::max()};
+  for (const auto& client : clientInfos) {
+    if (client.second.id != id &&
+        client.second.gameID == clientInfos[id].gameID &&
+        client.second.partnerID == 0 &&
+        uint32_t(abs(int64_t(client.second.level) - int64_t(clientInfos[id].level))) < minAbs) {
+      match = client.second.id;
+      minAbs = uint32_t(abs(int64_t(client.second.level) - int64_t(clientInfos[id].level)));
+    }
+  }
+  if (match == 0) return;
+  pair(id, match);
+}
+
+
+std::vector<ClientInfo> GGS::getClientInfo() {
+  std::vector<ClientInfo> result;
+  ciMutex.lock();
+  for (const auto& client : clientInfos) {
+    result.push_back(client.second);
+  }
+  ciMutex.unlock();
+  return result;
+}
+  
+int64_t GGS::uptime() const {
+  auto currentTime = Clock::now();
+  return std::chrono::duration_cast<std::chrono::seconds>(currentTime-startTime).count();
+}
--- cpp/OpenGL/25_GenericGameServer/main.cpp
+++ cpp/OpenGL/25_GenericGameServer/main.cpp
@@ -1,174 +1,8 @@
 #include <iostream>
-#include <sstream>
 #include <memory>
-#include <limits>
-#include <map>
 #include <vector>
-#include <chrono>
 
-#include <Server.h>
-#include <NetCommon.h>
-
-#include "NetGame.h"
-
-class ClientInfo {
-public:
-  uint32_t id{0};
-  std::string name{""};
-  GameIDs gameID{GameIDs::InvalidID};
-  uint32_t level{0};
-  bool paired{false};
-  uint32_t partnerID{0};
-  
-  ClientInfo() {}
-  
-  ClientInfo(uint32_t id, const std::string& name, GameIDs gameID, uint32_t level) :
-    id{id},
-    name{cleanupName(name)},
-    gameID{gameID},
-    level{level},
-    paired{false},
-    partnerID{0}
-  {}
-
-  virtual ~ClientInfo() {}
-  
-  static std::string cleanupName(const std::string& name) {
-    std::string cName{name};
-    for (size_t i = 0;i<name.length();++i) {
-      cName[i] = cleanupChar(name[i]);
-    }
-    return cName;
-  }
-
-private:
-  static char cleanupChar(char c) {
-    std::string validChars{"01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ(),._ ;:"};
-    if (validChars.find(c) != std::string::npos) return c; else return '_';
-  }
-
-};
-
-typedef std::chrono::high_resolution_clock Clock;
-
-class GGS : public Server {
-public:
-  bool showErrors{false};
-  
-  GGS(short port) :
-    Server(port)
-  {
-  }
-  
-  virtual ~GGS() {
-  }
-  
-  virtual void handleClientConnection(uint32_t id) override {
-  }
-  
-  virtual void handleClientDisconnection(uint32_t id) override {
-    ciMutex.lock();
-
-    const uint32_t partnerID = clientInfos[id].partnerID;    
-    if (partnerID != 0) {
-      LostUserMessage l;
-      l.userID = id;
-      sendMessage(l.toString(), partnerID);
-    }
-    clientInfos.erase(id);
-    searchForMatch(partnerID);
-    ciMutex.unlock();
-  }
-  
-  void pair(uint32_t a, uint32_t b) {
-    clientInfos[a].partnerID = b;
-    clientInfos[b].partnerID = a;
-
-    PairedMessage l;
-    l.userID = a;
-    sendMessage(l.toString(), b);
-
-    l.userID = b;
-    sendMessage(l.toString(), a);
-  }
-  
-  void searchForMatch(uint32_t id) {
-    if (clientInfos.size() < 2) return;
-    
-    uint32_t match{0};
-    uint32_t minAbs{std::numeric_limits<uint32_t>::max()};
-    for (const auto& client : clientInfos) {
-      if (client.second.id != id &&
-          client.second.gameID == clientInfos[id].gameID &&
-          client.second.partnerID == 0 &&
-          uint32_t(abs(int64_t(client.second.level) - int64_t(clientInfos[id].level))) < minAbs) {
-        match = client.second.id;
-        minAbs = uint32_t(abs(int64_t(client.second.level) - int64_t(clientInfos[id].level)));
-      }
-    }
-    if (match == 0) return;
-    pair(id, match);
-  }
-  
-  virtual void handleClientMessage(uint32_t id, const std::string& message) override {
-    ciMutex.lock();
-    try {
-      MessageType pt = identifyString(message);
-      switch (pt) {
-        case MessageType::ConnectMessage : {
-          ConnectMessage r(message);
-          r.userID = id;
-          ClientInfo ci{id, r.name, r.gameID, r.level};
-          clientInfos[id] = ci;
-          searchForMatch(id);
-          break;
-        }
-        case MessageType::ReadyForNewMessage : {
-          ReadyForNewMessage r(message);
-          clientInfos[id].partnerID = 0;
-          break;
-        }
-        case MessageType::GameMessage : {
-          GameMessage g(message);
-          g.userID = id;
-          sendMessage(g.toString(), clientInfos[id].partnerID);
-          break;
-        }
-        default:
-          break;
-      };
-    } catch (const MessageException& e) {
-      if (showErrors) {
-        std::cerr << "MessageException: " << e.what() << std::endl;
-      }
-    }
- 
-    ciMutex.unlock();
-  }
-  
-  std::vector<ClientInfo> getClientInfo() {
-    std::vector<ClientInfo> result;
-    ciMutex.lock();
-    for (const auto& client : clientInfos) {
-      result.push_back(client.second);
-    }
-    ciMutex.unlock();
-    return result;
-  }
-    
-  int64_t uptime() const {
-    auto currentTime = Clock::now();
-    return std::chrono::duration_cast<std::chrono::seconds>(currentTime-startTime).count();
-  }
-
-  
-private:
-  std::map<uint32_t, ClientInfo> clientInfos;
-  std::mutex ciMutex;
-  
-  std::chrono::time_point<Clock> lastime = Clock::now();
-  std::chrono::time_point<Clock> startTime = Clock::now();
-};
+#include "GGS.h"
 
 static void globalExceptionHandler () {
   std::cerr << "terminate called after throwing an instance of ";
@@ -212,6 +46,7 @@ int main(int argc, char ** argv) {
         case '?' :
           std::cout << "? : help\n";
           std::cout << "l : list active users\n";
+          std::cout << "w : toggle logging to file\n";
           std::cout << "s : statisics\n";
           std::cout << "e : toggle message error display\n";
           std::cout << "q : quit server\n";
@@ -225,6 +60,16 @@ int main(int argc, char ** argv) {
         }
         case 's' : {
           std::cout << "Server Uptime: "<< s.uptime() << " sec.\n";
+          break;
+        }
+        case 'w' : {
+          if (s.logFile.empty()) {
+            s.logFile = "server.log";
+            std::cout << "Logging is enabled\n";
+          } else {
+            s.logFile = "";
+            std::cout << "Logging is disabled\n";
+          }
           break;
         }
         case 'e' : {
