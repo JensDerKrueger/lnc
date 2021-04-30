6defd79f00c956aa8552be7f35a2e16fdc448fdb
Jens Krüger <jens.krueger@uni-due.de>
2021-04-30 13:13:48 +0200

    fix index

--- cpp/OpenGL/Vis_01_IP/main.cpp
+++ cpp/OpenGL/Vis_01_IP/main.cpp
@@ -79,7 +79,8 @@ public:
     for (uint32_t y = 0;y<image.height;y+=4) {
       for (uint32_t x = 0;x<image.width;x+=4) {
         const uint8_t v = getLumiValue(x,image.height-y);
-        ss << lut[(v*lut.length())/255] << lut[(v*lut.length())/255];
+        const unsigned char c = lut[(v*(lut.length()-1))/255];
+        ss << c << c;
       }
       ss << "\n";
     }
