33dd47442acd2650dfbbe429a93ea039fb1938da
Jens Krüger <jens.krueger@uni-due.de>
2021-05-24 17:06:20 +0200

    generate texure once and set aspect ratio properly

--- cpp/OpenGL/34_Mosaic/main.cpp
+++ cpp/OpenGL/34_Mosaic/main.cpp
@@ -9,20 +9,32 @@ public:
   FontRenderer fr{"helvetica_neue.bmp", "helvetica_neue.pos"};
   std::shared_ptr<FontEngine> fe{nullptr};
   std::string text;
+  std::shared_ptr<GLTexture2D> result{nullptr};
+  
   
   virtual void init() {
     glEnv.setTitle("Mosaic Maker");
     fe = fr.generateFontEngine();
     mm.generate();
-
+    result = std::make_shared<GLTexture2D>(mm.getResultImage(2048),
+                                           GL_LINEAR, GL_LINEAR,
+                                           GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
     GL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
     GL(glBlendEquation(GL_FUNC_ADD));
     GL(glEnable(GL_BLEND));
+    GL(glClearColor(0,0,0,0));
   }
   
   virtual void draw() {
-    setDrawTransform(Mat4::scaling(1, -1, 1));
-    drawImage(mm.getResultImage());
+    GL(glClear(GL_COLOR_BUFFER_BIT));
+    float imageAspect = float(mm.getResultImage().width) /
+                        float(mm.getResultImage().height);    
+    float scale = imageAspect/glEnv.getFramebufferSize().aspect();
+    if (scale < 1)
+      setDrawTransform(Mat4::scaling(1, -1, 1) * Mat4::scaling(scale, 1, 1)  );
+    else
+      setDrawTransform(Mat4::scaling(1, -1, 1) * Mat4::scaling(1, 1/scale, 1)  );
+    drawImage(*result);
   }
 };
 
