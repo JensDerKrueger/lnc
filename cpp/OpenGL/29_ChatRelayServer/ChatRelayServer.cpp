ca218b691e23a7d4e510692501ea39e8531ff9dc
Harasz2020 <67601357+Harasz2020@users.noreply.github.com>
2021-05-29 20:52:15 +0200

    big VS-files Update
    - Added missing VS Projects
    - Added some missing .gitignore files
    - Changed VS Project files to match the general file structure
    -> now nearly all LNC projects have VS-Projects that are tested against all 4 Configs (x64,x86,release,debug)

--- cpp/OpenGL/29_ChatRelayServer/ChatRelayServer.cpp
+++ cpp/OpenGL/29_ChatRelayServer/ChatRelayServer.cpp
@@ -92,7 +92,7 @@ std::string FrontendServer::getTimeStr() const {
   auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
   std::stringstream ss;
-  ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+  ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");  
   return ss.str();
 }
 
