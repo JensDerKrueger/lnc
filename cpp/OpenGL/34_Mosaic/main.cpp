cb37b4df77dca209408abd78d247d9d52214ba53
Jens Krüger <jens.krueger@uni-due.de>
2021-05-31 00:25:37 +0200

    adjust demo code

--- cpp/OpenGL/34_Mosaic/main.cpp
+++ cpp/OpenGL/34_Mosaic/main.cpp
@@ -6,7 +6,7 @@
 
 class MyGLApp : public GLApp {
 public:
-//  MosaicMaker mm{"smallImages", "largeImage.bmp", {100,100}, {10,10}};
+//  MosaicMaker mm{"smallImages", "largeImage.bmp", 100, {10,10}, {4,7}};
   MosaicMaker mm{"/Users/lnc/lnc/cpp/OpenGL/34_Mosaic/smallImages",
                  "/Users/lnc/lnc/cpp/OpenGL/34_Mosaic/jens.bmp",
                  50, {5,5}, {4,7}};
