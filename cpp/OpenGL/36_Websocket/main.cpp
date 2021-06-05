16483393395da330a62f8560082cd5cbb1bed9ed
Late Night Coding <jens.krueger@uni-due.de>
2021-06-06 00:51:30 +0200

    basic string processing

--- cpp/OpenGL/36_Websocket/main.cpp
+++ cpp/OpenGL/36_Websocket/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cctype>
 
 #include <Server.h>
+#include <SHA1.h>
 
 
 class MyClientConnection : public BaseClientConnection {
@@ -10,30 +14,94 @@ public:
   virtual ~MyClientConnection() {}
   
 protected:
-  std::vector<int8_t> recievedBytes;
+  bool handshakeComplete{false};
+  std::vector<int8_t> receivedBytes;
   virtual std::string handleIncommingData(int8_t* data, uint32_t bytes) override {
-    recievedBytes.insert(recievedBytes.end(), data, data+bytes);
-    if (recievedBytes.size() > 3) {
-      for (uint32_t i = 0;i<recievedBytes.size()-3;++i) {
-        if ((int)recievedBytes[i] == 13 && (int)recievedBytes[i+1] == 10 &&
-            (int)recievedBytes[i+2] == 13 && (int)recievedBytes[i+3] == 10) {
-          std::stringstream ss;
-          if (i > 0) {
-            for (uint32_t j = 0;j<i;++j) {
-              ss << recievedBytes[j];
+    if (handshakeComplete) {
+      
+    } else {
+      receivedBytes.insert(receivedBytes.end(), data, data+bytes);
+      if (receivedBytes.size() > 3) {
+        for (uint32_t i = 0;i<receivedBytes.size()-3;++i) {
+          if ((int)receivedBytes[i] == 13 && (int)receivedBytes[i+1] == 10 &&
+              (int)receivedBytes[i+2] == 13 && (int)receivedBytes[i+3] == 10) {
+            std::stringstream ss;
+            if (i > 0) {
+              for (uint32_t j = 0;j<i;++j) {
+                ss << receivedBytes[j];
+              }
             }
+            receivedBytes.erase(receivedBytes.begin(), receivedBytes.begin()+long(i+4));
+            handleHandshake(ss.str());
+            return "";
           }
-          recievedBytes.erase(recievedBytes.begin(), recievedBytes.begin()+long(i+4));
-          return ss.str();
         }
       }
     }
+    
     return "";
   }
   
   virtual void sendMessage(std::string message) override {
   }
 
+private:
+  
+  std::string trimLeft(const std::string& input) {
+    std::string result = input;
+    result.erase(result.begin(), std::find_if(result.begin(), result.end(), [](unsigned char ch) {return !std::isspace(ch);}));
+    return result;
+  }
+
+  std::string trimRight(const std::string& input) {
+    std::string result = input;
+    result.erase(std::find_if(result.rbegin(), result.rend(), [](unsigned char ch) { return !std::isspace(ch); }).base(), result.end());
+    return result;
+  }
+
+  std::string trim(const std::string& input) {
+    return trimLeft(trimRight(input));
+  }
+  
+  std::string toLower(const std::string& input) {
+    std::string result = input;
+    std::transform(input.begin(), input.end(), result.begin(), [](int c) { return std::tolower(c); });
+    return result;
+  }
+
+  std::string toUpper(const std::string& input) {
+    std::string result = input;
+    std::transform(input.begin(), input.end(), result.begin(), [](int c) { return std::toupper(c); });
+    return result;
+  }
+  
+  std::vector<std::string> tokenizer(const std::string& input, const std::string& delim) {
+    char* cstring = strdup(input.c_str());
+    char* token = std::strtok(cstring, delim.c_str());
+    std::vector<std::string> result;
+    while (token) {
+      result.push_back(token);
+      token = std::strtok(nullptr,  delim.c_str());
+    }
+    free(cstring);
+    return result;
+  }
+  
+  void handleHandshake(const std::string& initialMessage) {
+    std::vector<std::string> lines = tokenizer(initialMessage, "\r\n");
+    
+    for (const std::string& line: lines) {
+      std::cout << "->" << line << "<-" << std::endl;
+      
+      std::vector<std::string> values = tokenizer(line, ":");
+      if (values.size() != 2) continue;
+      
+      if ("sec-websocket-key" == toLower(values[0])) {
+        std::cout << "  ->" << trim(values[1]) << "<-" << std::endl;
+      }
+    }
+  }
+  
 };
 
 class EchoServer : public Server<MyClientConnection> {
@@ -62,3 +130,6 @@ int main(int argc, char ** argv) {
   
   return EXIT_SUCCESS;
 }
+
+// 258EAFA5-E914-47DA-95CA-C5AB0DC85B11
+
