1e086ee6476f2ce57caa70fa2b0bdfa706d83ed2
Jens Krüger <jens.krueger@uni-due.de>
2021-06-06 23:41:44 +0200

    add startswith and endswith

--- cpp/OpenGL/Utils/StringTools.h
+++ cpp/OpenGL/Utils/StringTools.h
@@ -9,3 +9,5 @@ std::string trim(const std::string& input);
 std::string toLower(const std::string& input);
 std::string toUpper(const std::string& input);
 std::vector<std::string> tokenize(const std::string& input, const std::string& delim);
+bool endsWith(std::string_view str, std::string_view suffix);
+bool startsWith(std::string_view str, std::string_view prefix);
