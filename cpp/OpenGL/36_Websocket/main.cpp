1f0fcdf1271bb933f129774b64becb7e40ede7ab
Jens Krüger <jens.krueger@uni-due.de>
2021-06-14 01:07:45 +0200

    simple paint example

--- cpp/OpenGL/36_Websocket/main.cpp
+++ cpp/OpenGL/36_Websocket/main.cpp
@@ -15,19 +15,24 @@ public:
   }
   
   virtual void handleClientMessage(uint32_t id, const std::string& message) override {
-    std::cout << "Str: " << message << std::endl;
-    sendMessage(message, id);
+    sendMessage(message);
   }
 
   virtual void handleClientMessage(uint32_t id, const std::vector<uint8_t>& message) override {
-    std::cout << "Bin: [" << std::hex;
-    for (size_t i = 0;i<message.size();++i) {
-      std::cout << "0x" << int(message[i]) << ((i<message.size()-1) ? ", " : "");
-    }
-    std::cout << "]" << std::dec << std::endl;
-    sendMessage(message, id);
+    sendMessage(message);
   }
 
+  virtual void handleClientConnection(uint32_t id, const std::string& address, uint16_t port) override {
+    std::cout << "New client (id:" << id << ") connected from " << address << std::endl;
+  }
+
+  virtual void handleClientDisconnection(uint32_t id) override {
+    std::cout << "Client (id:" << id << ") disconnected" << std::endl;
+  }
+  
+  virtual void handleProtocolMessage(uint32_t id, uint32_t messageID, const std::vector<uint8_t>& message) override {
+    std::cout << "Protocol Message 0x" << std::hex << messageID << std::dec << " form id " << id << std::endl;
+  }
 
 };
 
@@ -35,8 +40,16 @@ public:
 int main(int argc, char ** argv) {
   EchoServer s(8899);
   s.start();
-  
-  while (true) {
+
+  std::cout << "Starting ";
+  while (s.isStarting()) {
+    std::cout << "." << std::flush;
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+
+  std::cout << "\nRunning" << std::endl;;
+  while (s.isOK()) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
   
   return EXIT_SUCCESS;
