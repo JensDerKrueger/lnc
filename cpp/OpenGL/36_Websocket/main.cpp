57c08038889d4a0c9ab8f87856f242eec6aae7ca
Jens Krüger <jens.krueger@uni-due.de>
2021-06-15 16:37:56 +0200

    paint on the server

--- cpp/OpenGL/36_Websocket/main.cpp
+++ cpp/OpenGL/36_Websocket/main.cpp
@@ -7,16 +7,19 @@
 
 constexpr const uint16_t canvasWidth = 500;
 constexpr const uint16_t canvasHeight = 500;
+constexpr const uint16_t brushWidth = 10;
+constexpr const uint16_t brushHeight = 10;
+
 
 class EchoServer : public Server<WebSocketConnection> {
 public:
   EchoServer(uint16_t port) :
   Server(port)
   {
+    imageMessage.resize(canvasWidth*canvasHeight*4+1);
   }
   
   virtual ~EchoServer() {
-    imageMessage.resize(canvasWidth*canvasHeight*4+1);
   }
   
   virtual void handleClientMessage(uint32_t id, const std::string& message) override {
@@ -24,21 +27,14 @@ public:
   }
 
   virtual void handleClientMessage(uint32_t id, const std::vector<uint8_t>& message) override {
+    paint(message);
     sendMessage(message);
   }
 
   virtual void handleClientConnection(uint32_t id, const std::string& address, uint16_t port) override {
     
     std::cout << "New client (id:" << id << ") connected from " << address << std::endl;
-    
-    imageMessage[0] = 0;
-    for (size_t i = 0;i<canvasWidth*canvasHeight;++i) {
-      imageMessage[1+i*4+0] = 200;
-      imageMessage[1+i*4+1] = 255;
-      imageMessage[1+i*4+2] = 200;
-      imageMessage[1+i*4+3] = 255;
-    }
-    
+    std::cout << "A total of " << getValidIDs().size() << " clinents are currently connected." << std::endl;
     sendMessage(imageMessage,id);
   }
 
@@ -55,8 +51,35 @@ public:
   }
   
 private:
-  
   std::vector<uint8_t> imageMessage;
+  
+  void paint(const std::vector<uint8_t>& message) {
+    if (message.size() == 8 || message[0] == 1) {
+      uint16_t brushCenterPosX = uint16_t(uint16_t(message[1]) << 8) | message[2];
+      uint16_t brushCenterPosY = uint16_t(uint16_t(message[3]) << 8) | message[4];
+      uint8_t r = message[5];
+      uint8_t g = message[6];
+      uint8_t b = message[7];
+            
+      for (int32_t y = 0;y<brushHeight;++y) {
+        for (int32_t x = 0;x<brushWidth;++x) {
+          
+          int32_t posX = brushCenterPosX+x;
+          int32_t posY = brushCenterPosY+y;
+          
+          if (posX >= 0 && posX < canvasWidth &&
+              posY >= 0 && posY < canvasHeight) {
+            
+            imageMessage[size_t((posX + posY * canvasWidth)*4+1+0)] = r;
+            imageMessage[size_t((posX + posY * canvasWidth)*4+1+1)] = g;
+            imageMessage[size_t((posX + posY * canvasWidth)*4+1+2)] = b;
+            imageMessage[size_t((posX + posY * canvasWidth)*4+1+3)] = 255;
+          }
+          
+        }
+      }
+    }
+  }
 };
 
 
