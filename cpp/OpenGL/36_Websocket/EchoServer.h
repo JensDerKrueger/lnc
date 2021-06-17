6ca9942a8bb9c69223ce10ae7c74a076524dde48
Jens Krüger <jens.krueger@uni-due.de>
2021-06-18 01:48:38 +0200

    remove artifact from xcode's extract method fail

--- cpp/OpenGL/36_Websocket/EchoServer.h
+++ cpp/OpenGL/36_Websocket/EchoServer.h
@@ -23,13 +23,10 @@ public:
   EchoServer(uint16_t port, uint16_t canvasWidth, uint16_t canvasHeight,
              const std::vector<std::string>& layerImages);
   virtual ~EchoServer();
-  
   void savePaintLayers();
 
 protected:
   virtual void handleClientMessage(uint32_t id, const std::string& message) override;
-  std::vector<uint8_t> extracted(const std::vector<uint8_t> &message);
-  
   virtual void handleClientMessage(uint32_t id, const std::vector<uint8_t>& message) override;
   virtual void handleClientConnection(uint32_t id, const std::string& address, uint16_t port) override;
   virtual void handleClientDisconnection(uint32_t id) override;
