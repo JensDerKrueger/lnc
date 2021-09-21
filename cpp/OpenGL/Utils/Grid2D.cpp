981dd8f5b0014dadd0deaae550a2fab39034ca40
Jens Krüger <jens.krueger@uni-due.de>
2021-09-21 12:39:21 +0200

    extend grid operators to handle different aspect ratios, fix indentation

--- cpp/OpenGL/Utils/Grid2D.cpp
+++ cpp/OpenGL/Utils/Grid2D.cpp
@@ -50,289 +50,361 @@ size_t Grid2D::getHeight() const {
 }
 
 std::string Grid2D::toString() const {
-    std::stringstream s;
-    for (size_t i = 0;i<data.size();++i) {
-        s << data[i];
-        if (i%width == width-1 && i != 0)
-            s << std::endl;
-        else
-            s << ", ";
-    }
-    return s.str();
+  std::stringstream s;
+  for (size_t i = 0;i<data.size();++i) {
+    s << data[i];
+    if (i%width == width-1 && i != 0)
+      s << std::endl;
+    else
+      s << ", ";
+  }
+  return s.str();
 }
 
 std::vector<uint8_t> Grid2D::toByteArray() const {
-    std::vector<uint8_t> uidata(data.size()*3);
-    for (size_t i = 0;i<data.size();++i) {
-        uidata[i*3+0] = uint8_t(data[i]*255);
-        uidata[i*3+1] = uint8_t(data[i]*255);
-        uidata[i*3+2] = uint8_t(data[i]*255);
-    }
-    return uidata;
+  std::vector<uint8_t> uidata(data.size()*3);
+  for (size_t i = 0;i<data.size();++i) {
+    uidata[i*3+0] = uint8_t(data[i]*255);
+    uidata[i*3+1] = uint8_t(data[i]*255);
+    uidata[i*3+2] = uint8_t(data[i]*255);
+  }
+  return uidata;
 }
 
 void Grid2D::setValue(size_t x, size_t y, float value) {
-    data[index(x,y)] = value;
+  data[index(x,y)] = value;
 }
 
 
 float Grid2D::getValueNormalized(float x, float y) const {
-    return data[index(size_t(x*width),size_t(y*height))];
+  return data[index(size_t(x*width),size_t(y*height))];
 }
 
 float Grid2D::getValue(size_t x, size_t y) const {
-    return data[index(x,y)];
+  return data[index(x,y)];
 }
 
 
 float Grid2D::sample(const Vec2& pos) const  {
-    return sample(pos.x, pos.y);
+  return sample(pos.x, pos.y);
 }
 
 float Grid2D::sample(float x, float y) const {
-    x = std::max(std::min(x,1.0f), 0.0f);
-    y = std::max(std::min(y,1.0f), 0.0f);
-    
-    float sx = x*(width-1);
-    float sy = y*(height-1);
-    
-    float alpha = sx - floorf(sx);
-    float beta  = sy - floorf(sy);
-    
-    Vec2ui a{uint32_t(floorf(sx)),uint32_t(floorf(sy))};
-    Vec2ui b{uint32_t(ceilf(sx)),uint32_t(floorf(sy))};
-    Vec2ui c{uint32_t(floorf(sx)),uint32_t(ceilf(sy))};
-    Vec2ui d{uint32_t(ceilf(sx)),uint32_t(ceilf(sy))};
-    
-    float va = getValue(a.x, a.y);
-    float vb = getValue(b.x, b.y);
-    float vc = getValue(c.x, c.y);
-    float vd = getValue(d.x, d.y);
-    
-    return (va * (1.0f-alpha) + vb * alpha) * (1.0f-beta) + (vc * (1.0f-alpha) + vd * alpha) * beta;
+  x = std::max(std::min(x,1.0f), 0.0f);
+  y = std::max(std::min(y,1.0f), 0.0f);
+  
+  float sx = x*(width-1);
+  float sy = y*(height-1);
+  
+  float alpha = sx - floorf(sx);
+  float beta  = sy - floorf(sy);
+  
+  Vec2ui a{uint32_t(floorf(sx)),uint32_t(floorf(sy))};
+  Vec2ui b{uint32_t(ceilf(sx)),uint32_t(floorf(sy))};
+  Vec2ui c{uint32_t(floorf(sx)),uint32_t(ceilf(sy))};
+  Vec2ui d{uint32_t(ceilf(sx)),uint32_t(ceilf(sy))};
+  
+  float va = getValue(a.x, a.y);
+  float vb = getValue(b.x, b.y);
+  float vc = getValue(c.x, c.y);
+  float vd = getValue(d.x, d.y);
+  
+  return (va * (1.0f-alpha) + vb * alpha) * (1.0f-beta) + (vc * (1.0f-alpha) + vd * alpha) * beta;
 }
 
 Vec3 Grid2D::normal(const Vec2& pos) const {
-    return normal(pos.x, pos.y);
+  return normal(pos.x, pos.y);
 }
 
 Vec3 Grid2D::normal(float x, float y) const {
-    x = std::max(std::min(x, 1.0f), 0.0f);
-    y = std::max(std::min(y, 1.0f), 0.0f);
+  x = std::max(std::min(x, 1.0f), 0.0f);
+  y = std::max(std::min(y, 1.0f), 0.0f);
 
-    float sx = x * (width - 1);
-    float sy = y * (height - 1);
-    
-    Vec2ui a{ uint32_t(floorf(sx)),uint32_t(floorf(sy)) };
-    Vec2ui b{ uint32_t(ceilf(sx)),uint32_t(floorf(sy)) };
-    Vec2ui c{ uint32_t(floorf(sx)),uint32_t(ceilf(sy)) };
-    Vec2ui d{ uint32_t(ceilf(sx)),uint32_t(ceilf(sy)) };
+  float sx = x * (width - 1);
+  float sy = y * (height - 1);
+  
+  Vec2ui a{ uint32_t(floorf(sx)),uint32_t(floorf(sy)) };
+  Vec2ui b{ uint32_t(ceilf(sx)),uint32_t(floorf(sy)) };
+  Vec2ui c{ uint32_t(floorf(sx)),uint32_t(ceilf(sy)) };
+  Vec2ui d{ uint32_t(ceilf(sx)),uint32_t(ceilf(sy)) };
 
-    float va = getValue(a.x, a.y);
-    float vb = getValue(b.x, b.y);
-    float vc = getValue(c.x, c.y);
-    float vd = getValue(d.x, d.y);
+  float va = getValue(a.x, a.y);
+  float vb = getValue(b.x, b.y);
+  float vc = getValue(c.x, c.y);
+  float vd = getValue(d.x, d.y);
 
-    Vec3 n1 = Vec3::cross(Vec3(1.0f/width,(vb-va), 0.0f), Vec3(0.0f,(vc-va), 1.0f/height ));
-    Vec3 n2 = Vec3::cross(Vec3(-1.0f/width,(vc - vd), 0.0f), Vec3(0.0f, (vb - vd), -1.0f/height));
+  Vec3 n1 = Vec3::cross(Vec3(1.0f/width,(vb-va), 0.0f), Vec3(0.0f,(vc-va), 1.0f/height ));
+  Vec3 n2 = Vec3::cross(Vec3(-1.0f/width,(vc - vd), 0.0f), Vec3(0.0f, (vb - vd), -1.0f/height));
 
-    
-    return Vec3::normalize((n1 + n2) / 2.0f);
+  return Vec3::normalize((n1 + n2) / 2.0f);
 }
 
 Grid2D Grid2D::genRandom(size_t x, size_t y, uint32_t seed) {
   Random myrandom{seed};
   Grid2D result{x,y};
   for (size_t i = 0;i<result.data.size();++i) {
-      result.data[i] = myrandom.rand01();
+    result.data[i] = myrandom.rand01();
   }
   return result;
 }
 
 Grid2D Grid2D::genRandom(size_t x, size_t y) {
-    Grid2D result{x,y};
-    for (size_t i = 0;i<result.data.size();++i) {
-        result.data[i] = staticRand.rand01();
-    }
-    return result;
+  Grid2D result{x,y};
+  for (size_t i = 0;i<result.data.size();++i) {
+    result.data[i] = staticRand.rand01();
+  }
+  return result;
 }
 
 Grid2D Grid2D::operator*(const float& value) const {
-    Grid2D result{width,height};
-    for (size_t i = 0;i<result.data.size();++i) {
-        result.data[i] = data[i]*value;
-    }
-    return result;
+  Grid2D result{width,height};
+  for (size_t i = 0;i<result.data.size();++i) {
+    result.data[i] = data[i]*value;
+  }
+  return result;
 }
 
 Grid2D Grid2D::operator+(const float& value) const {
-    Grid2D result{width,height};
-    for (size_t i = 0;i<result.data.size();++i) {
-        result.data[i] = data[i]+value;
-    }
-    return result;
+  Grid2D result{width,height};
+  for (size_t i = 0;i<result.data.size();++i) {
+    result.data[i] = data[i]+value;
+  }
+  return result;
 }
 
 Grid2D Grid2D::operator-(const float& value) const {
-    Grid2D result{width,height};
-    for (size_t i = 0;i<result.data.size();++i) {
-        result.data[i] = data[i]-value;
-    }
-    return result;
+  Grid2D result{width,height};
+  for (size_t i = 0;i<result.data.size();++i) {
+    result.data[i] = data[i]-value;
+  }
+  return result;
 }
 
 Grid2D Grid2D::operator/(const float& value) const {
-    return *this * (1.0f/value);
+  return *this * (1.0f/value);
 }
 
+std::pair<size_t,size_t> Grid2D::findMaxSize(const Grid2D& other) const {
+  return std::make_pair(std::max(width, other.width),
+                        std::max(height, other.height));
+}
+
+
 Grid2D Grid2D::operator-(const Grid2D& other) const {
-  // TODO: add check if aspect ratio matches, throw exception otherwise
-    if (other.width > width) {
-        Grid2D result{other.width,other.height};
-        size_t i=0;
-        for (size_t y = 0;y<other.height;++y) {
-            for (size_t x = 0;x<other.width;++x) {
-                result.data[i] = other.data[i] - sample(x/float(other.width-1.0f),y/float(other.height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else if (other.width < width) {
-        Grid2D result{width,height};
-        size_t i=0;
-        for (size_t y = 0;y<height;++y) {
-            for (size_t x = 0;x<width;++x) {
-                result.data[i] = data[i] - other.sample(x/float(width-1.0f),y/float(height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else {
-        Grid2D result{width,height};
-        for (size_t i = 0;i<result.data.size();++i) {
-            result.data[i] = data[i]-other.data[i];
-        }
-        return result;
+  std::pair<size_t,size_t> maxDims = findMaxSize(other);
+  Grid2D result{maxDims.first,maxDims.second};
+  
+  if (other.width == width && other.height == height) {
+    for (size_t i = 0;i<result.data.size();++i) {
+      result.data[i] = data[i] - other.data[i];
     }
+    return result;
+  }
+    
+  size_t i=0;
+
+  if (maxDims.first == width && maxDims.second == height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = data[i] - other.sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+  
+  if (maxDims.first == other.width && maxDims.second == other.height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = other.data[i] - sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+
+  for (size_t y = 0;y<maxDims.second;++y) {
+    const float normY = y/float(maxDims.second-1.0f);
+    for (size_t x = 0;x<maxDims.first;++x) {
+      const float normX = x/float(maxDims.first-1.0f);
+      result.data[i] = other.sample(normX, normY) - sample(normX,normY);
+      i++;
+    }
+  }
+  return result;
 }
 
 Grid2D Grid2D::operator*(const Grid2D& other) const {
-  // TODO: add check if aspect ratio matches, throw exception otherwise
-    if (other.width > width) {
-        Grid2D result{other.width,other.height};
-        size_t i=0;
-        for (size_t y = 0;y<other.height;++y) {
-            for (size_t x = 0;x<other.width;++x) {
-                result.data[i] = other.data[i] * sample(x/float(other.width-1.0f),y/float(other.height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else if (other.width < width) {
-        Grid2D result{width,height};
-        size_t i=0;
-        for (size_t y = 0;y<height;++y) {
-            for (size_t x = 0;x<width;++x) {
-                result.data[i] = data[i] * other.sample(x/float(width-1.0f),y/float(height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else {
-        Grid2D result{width,height};
-        for (size_t i = 0;i<result.data.size();++i) {
-            result.data[i] = data[i]*other.data[i];
-        }
-        return result;
+  std::pair<size_t,size_t> maxDims = findMaxSize(other);
+  Grid2D result{maxDims.first,maxDims.second};
+  
+  if (other.width == width && other.height == height) {
+    for (size_t i = 0;i<result.data.size();++i) {
+      result.data[i] = data[i] * other.data[i];
     }
+    return result;
+  }
+    
+  size_t i=0;
+
+  if (maxDims.first == width && maxDims.second == height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = data[i] * other.sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+  
+  if (maxDims.first == other.width && maxDims.second == other.height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = other.data[i] * sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+
+  for (size_t y = 0;y<maxDims.second;++y) {
+    const float normY = y/float(maxDims.second-1.0f);
+    for (size_t x = 0;x<maxDims.first;++x) {
+      const float normX = x/float(maxDims.first-1.0f);
+      result.data[i] = other.sample(normX, normY) * sample(normX,normY);
+      i++;
+    }
+  }
+  return result;
 }
 
 Grid2D Grid2D::operator/(const Grid2D& other) const {
-  // TODO: add check if aspect ratio matches, throw exception otherwise
-    if (other.width > width) {
-        Grid2D result{other.width,other.height};
-        size_t i=0;
-        for (size_t y = 0;y<other.height;++y) {
-            for (size_t x = 0;x<other.width;++x) {
-                result.data[i] = other.data[i] / sample(x/float(other.width-1.0f),y/float(other.height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else if (other.width < width) {
-        Grid2D result{width,height};
-        size_t i=0;
-        for (size_t y = 0;y<height;++y) {
-            for (size_t x = 0;x<width;++x) {
-                result.data[i] = data[i] / other.sample(x/float(width-1.0f),y/float(height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else {
-        Grid2D result{width,height};
-        for (size_t i = 0;i<result.data.size();++i) {
-            result.data[i] = data[i]/other.data[i];
-        }
-        return result;
+  std::pair<size_t,size_t> maxDims = findMaxSize(other);
+  Grid2D result{maxDims.first,maxDims.second};
+  
+  if (other.width == width && other.height == height) {
+    for (size_t i = 0;i<result.data.size();++i) {
+      result.data[i] = data[i] / other.data[i];
     }
+    return result;
+  }
+    
+  size_t i=0;
+
+  if (maxDims.first == width && maxDims.second == height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = data[i] / other.sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+  
+  if (maxDims.first == other.width && maxDims.second == other.height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = other.data[i] / sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+
+  for (size_t y = 0;y<maxDims.second;++y) {
+    const float normY = y/float(maxDims.second-1.0f);
+    for (size_t x = 0;x<maxDims.first;++x) {
+      const float normX = x/float(maxDims.first-1.0f);
+      result.data[i] = other.sample(normX, normY) / sample(normX,normY);
+      i++;
+    }
+  }
+  return result;
 }
 
 Grid2D Grid2D::operator+(const Grid2D& other) const {
-  // TODO: add check if aspect ratio matches, throw exception otherwise
+  std::pair<size_t,size_t> maxDims = findMaxSize(other);
+  Grid2D result{maxDims.first,maxDims.second};
   
-    if (other.width > width) {
-        Grid2D result{other.width,other.height};
-        size_t i=0;
-        for (size_t y = 0;y<other.height;++y) {
-            for (size_t x = 0;x<other.width;++x) {
-                result.data[i] = other.data[i] + sample(x/float(other.width-1.0f),y/float(other.height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else if (other.width < width) {
-        Grid2D result{width,height};
-        size_t i=0;
-        for (size_t y = 0;y<height;++y) {
-            for (size_t x = 0;x<width;++x) {
-                result.data[i] = data[i] + other.sample(x/float(width-1.0f),y/float(height-1.0f));
-                i++;
-            }
-        }
-        return result;
-    } else {
-        Grid2D result{width,height};
-        for (size_t i = 0;i<result.data.size();++i) {
-            result.data[i] = data[i]+other.data[i];
-        }
-        return result;
+  if (other.width == width && other.height == height) {
+    for (size_t i = 0;i<result.data.size();++i) {
+      result.data[i] = data[i] + other.data[i];
     }
+    return result;
+  }
+    
+  size_t i=0;
+  if (maxDims.first == width && maxDims.second == height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = data[i] + other.sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+  
+  if (maxDims.first == other.width && maxDims.second == other.height) {
+    for (size_t y = 0;y<maxDims.second;++y) {
+      const float normY = y/float(maxDims.second-1.0f);
+      for (size_t x = 0;x<maxDims.first;++x) {
+        const float normX = x/float(maxDims.first-1.0f);
+        result.data[i] = other.data[i] + sample(normX,normY);
+        i++;
+      }
+    }
+    return result;
+  }
+
+  for (size_t y = 0;y<maxDims.second;++y) {
+    const float normY = y/float(maxDims.second-1.0f);
+    for (size_t x = 0;x<maxDims.first;++x) {
+      const float normX = x/float(maxDims.first-1.0f);
+      result.data[i] = other.sample(normX, normY) + sample(normX,normY);
+      i++;
+    }
+  }
+  return result;
 }
     
 void Grid2D::normalize(const float maxVal) {
-    if (data.empty()) return;
-    
-    float minValue = data[0];
-    float maxValue = data[0];
-    for (size_t i = 0;i<data.size();++i) {
-        minValue = std::min(minValue, data[i]);
-        maxValue = std::max(maxValue, data[i]);
-    }
-    
-    const float scale = maxVal/(maxValue-minValue);
-    for (size_t i = 0;i<data.size();++i) {
-        data[i] = (data[i]-minValue) * scale;
-    }
+  if (data.empty()) return;
+  
+  float minValue = data[0];
+  float maxValue = data[0];
+  for (size_t i = 0;i<data.size();++i) {
+    minValue = std::min(minValue, data[i]);
+    maxValue = std::max(maxValue, data[i]);
+  }
+  
+  const float scale = maxVal/(maxValue-minValue);
+  for (size_t i = 0;i<data.size();++i) {
+    data[i] = (data[i]-minValue) * scale;
+  }
 }
 
 Vec2t<size_t> Grid2D::maxValue() const {
   float maxVal = std::numeric_limits<float>::min();
   Vec2t<size_t> maxV{0,0};
   for (size_t i = 0;i<data.size();++i) {
-    if (maxVal < data[i]){
+    if (maxVal < data[i]) {
       maxVal = data[i];
-      maxV = Vec2t<size_t>{size_t(i % width), size_t(i / width)};    }
+      maxV = Vec2t<size_t>{size_t(i % width), size_t(i / width)};
+    }
   }
   return maxV;
 }
@@ -350,27 +422,27 @@ Vec2t<size_t> Grid2D::minValue() const {
 }
 
 size_t Grid2D::index(size_t x, size_t y) const {
-    return x + y * width;
+  return x + y * width;
 }
 
 std::ostream& operator<<(std::ostream &os, const Grid2D& v) {
-    os << v.toString() ; return os;
+  os << v.toString() ; return os;
 }
 
 Grid2D Grid2D::fromBMP(const std::string& filename) {
-    Image bmp = BMP::load(filename);
-    
-    Grid2D g{bmp.width, bmp.height};
-    
-    size_t i = 0;
-    for (size_t y = 0;y<g.height;++y) {
-        for (size_t x = 0;x<g.width;++x) {
-            g.data[i] = bmp.getValue(uint32_t(x),uint32_t(y),0)/255.0f;
-            i++;
-        }
+  Image bmp = BMP::load(filename);
+  
+  Grid2D g{bmp.width, bmp.height};
+  
+  size_t i = 0;
+  for (size_t y = 0;y<g.height;++y) {
+    for (size_t x = 0;x<g.width;++x) {
+      g.data[i] = bmp.getValue(uint32_t(x),uint32_t(y),0)/255.0f;
+      i++;
     }
+  }
 
-    return g;
+  return g;
 }
 
 void Grid2D::fill(float value) {
